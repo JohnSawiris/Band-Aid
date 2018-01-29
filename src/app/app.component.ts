d306e4785e5575ff615fa0f2a0a5047297f2d96c
Adam <alspdx@gmail.com>
2018-01-29 13:31:33 -0800

    add service to get information from discogs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { DiscogsService } from './discogs.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [DiscogsService]
 })
+
 export class AppComponent {
   title = 'app';
+  searchList: any[] = null;
+
+  constructor(
+    private discogsService: DiscogsService
+  ) { }
+
+  getSearchList(query: string) {
+    this.discogsService.searchByQuery(query).subscribe(response => {
+      this.searchList = response.json().results;
+      console.log(this.searchList);
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +10,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+}
