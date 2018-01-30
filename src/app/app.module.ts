a0586d62b6ef7923b7dbbfb9493de41ea220627e
Guest User <Guest@Epicodus-3E.local>
2018-01-29 16:29:16 -0800

    add search component and result list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ResultsListComponent } from './results-list/results-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ResultsListComponent
   ],
   imports: [
     BrowserModule,
