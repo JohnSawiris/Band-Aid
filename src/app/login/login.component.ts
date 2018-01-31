f7fef7013193c518249a7c0bf0a729ee4f6a09d4
Adam <alspdx@gmail.com>
2018-01-31 14:26:56 -0800

    update header component, login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [AuthenticationService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService: AuthenticationService,
+    private router: Router
+  ) {
+    this.authService.user.subscribe(user => {
+      if(user) {
+        this.router.navigate(['user-profile/', user.uid]);
+      }
+    });
+  }
 
   ngOnInit() {
+    this.authService.login();
   }
 
 }
