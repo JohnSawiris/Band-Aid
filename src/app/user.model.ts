70ebeddc32380182521705f3b07e0c75249747f0
Mikey Pinaud and Tyler Bussanich <tyler@>
2018-01-31 14:41:47 -0800

    update album.model

--- src/app/user.model.ts
+++ src/app/user.model.ts
@@ -3,10 +3,10 @@ import { Album } from './album.model'
 export class UserProfile {
   wishlistAlbums: Album[] = [];
   collectionAlbums: Album[] = [];
-  
+
   constructor (
     public name: string,
     public id: string,
-    public email: string,
-  ){ }
+    public email: string
+  ) { }
 }
